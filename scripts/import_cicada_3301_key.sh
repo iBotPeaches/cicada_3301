9bde1652db146293b8bf901103adda658cee93c4
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-20 17:32:32 -0500

    chore: gpg debug

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 key="$(pwd)/messages/67F363C61BA8FB6FDBA9C47D0670B0E57A35090F.asc"
-gpg --import "$key"
 
-if gpg --import "$key"; then
+if gpg -v --import "$key"; then
     echo "GPG key import successful!"
 else
     echo "GPG key import failed."
