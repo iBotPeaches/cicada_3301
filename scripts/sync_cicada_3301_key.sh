5f72da65fef883a6b31e54d975994ddaa835e36c
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-20 17:27:52 -0500

    chore: debug mode

--- scripts/sync_cicada_3301_key.sh
+++ scripts/sync_cicada_3301_key.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-gpg --recv-keys --keyserver hkps://keyserver.ubuntu.com 67F363C61BA8FB6FDBA9C47D0670B0E57A35090F
-gpg --list-keys
+gpg --allow-non-selfsigned-uid --recv-keys --keyserver hkps://keyserver.ubuntu.com 67F363C61BA8FB6FDBA9C47D0670B0E57A35090F
