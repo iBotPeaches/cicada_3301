368f933bdede1a613679afe77fba26600a376189
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-21 06:28:35 -0500

    build: debug import script

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -2,9 +2,13 @@
 
 set -x
 
+gpg --version
+
+gpg2 --version
+
 key="$(pwd)/messages/67F363C61BA8FB6FDBA9C47D0670B0E57A35090F.asc"
 
-if gpg --import-options show-only --import "$key"; then
+if gpg --allow-non-selfsigned-uid --import-options show-only --import "$key"; then
 
     if [ "$CI" = "true" ]; then
         for fpr in $(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | sort -u); do
