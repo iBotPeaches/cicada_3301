c1b260cea5e7623598ea1696359bfd670446afae
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-21 06:06:01 -0500

    chore: auto trust

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -3,6 +3,9 @@
 key="$(pwd)/messages/67F363C61BA8FB6FDBA9C47D0670B0E57A35090F.asc"
 
 if gpg --import-options show-only --import "$key"; then
+    gpg --list-keys --fingerprint |grep pub -A 1|grep -Ev "pub|--"|tr -d ' ' \
+    | awk 'BEGIN { FS = "\n" } ; { print $1":6:" } ' | gpg --import-ownertrust
+
     echo "GPG key import successful!"
 else
     echo "GPG key import failed."
