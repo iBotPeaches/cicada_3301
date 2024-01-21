a8a4f3f7728b2fa14cfcc44005331b6c23256334
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-21 06:14:40 -0500

    build: debug import script

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+set -x
+
 key="$(pwd)/messages/67F363C61BA8FB6FDBA9C47D0670B0E57A35090F.asc"
 
 if gpg --import-options show-only --import "$key"; then
-    for fpr in $(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | sort -u); do
-      echo -e "y\ny\n" |  gpg --command-fd 0 --expert --edit-key "$fpr" sign;
-    done
 
-    gpg --list-keys
+    if [ "$CI" = "true" ]; then
+        for fpr in $(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | sort -u); do
+          echo -e "y\ny\n" |  gpg --command-fd 0 --expert --edit-key "$fpr" sign;
+        done
+
+        gpg --list-keys
+    fi
 
     echo "GPG key import successful!"
 else
