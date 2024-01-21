94fa182a4e26404699ad4fff42b6bbcc17f387d7
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-21 16:49:06 -0500

    build: remove unneeded dump of key info

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -3,11 +3,6 @@
 set -x
 
 if gpg2 --keyserver hkps://keyserver.ubuntu.com --recv-keys 67F363C61BA8FB6FDBA9C47D0670B0E57A35090F; then
-
-    if [ "$CI" = "true" ]; then
-        gpg -k
-    fi
-
     echo "GPG key import successful!"
 else
     echo "GPG key import failed."
