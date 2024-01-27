7502b0f5b7273777fe28e30ac3e159e03ad66d27
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-01-27 18:30:51 -0500

    fix: add proper support for 'S' - ᛋ

--- tool/tests/Unit/LiberPrimusTest.php
+++ tool/tests/Unit/LiberPrimusTest.php
@@ -19,16 +19,24 @@ class LiberPrimusTest extends TestCase
     public static function dataProvider(): array
     {
         return [
-            //region 01.jpg
+            // region 01.jpg
             '01.jpg - line 1' => [
                 'runes' => 'ᚱ ᛝᚱᚪᛗᚹ ᛄᛁᚻᛖᛁᛡᛁ ᛗᚫᚣᚹ ᛠᚪᚫᚾ',
                 'sentence' => 'R [NG|ING]RAMW JIHEI[IA|IO]I MAEYW EAAAEN',
             ],
             '01.jpg - line 2' => [
                 'runes' => 'ᚣᛖᛈ ᛄᚫᚫᛞᛁᛉᛞᛁᛋᛇ ᛝᛚᚱᛇ ᚦᚫᛡ',
-                'sentence' => 'YEP JAEAEDIXDI[?]EO [NG|ING]LREO THAE[IA|IO]',
+                'sentence' => 'YEP JAEAEDIXDI[S|Z]EO [NG|ING]LREO THAE[IA|IO]',
             ],
-            //endregion
+            // endregion
+
+            // region 05.jpg
+            '05.jpg - line 1' => [
+                'runes' => 'ᛋᚩᛗᛖ ᚹᛁᛋᛞᚩᛗ ᚦᛖ ᛈᚱᛁᛗᛖᛋ ᚪᚱᛖ ᛋᚪᚳ',
+                'sentence' => '[S|Z]OME WI[S|Z]DOM THE PRIME[S|Z] ARE [S|Z]A[C|K]',
+            ],
+
+            // endregion
         ];
     }
 }
