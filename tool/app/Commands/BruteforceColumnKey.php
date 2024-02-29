cc9fa8c2e1f6d14e4a7a2e31c1eca7b814a3ca20
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-02-29 06:57:02 -0500

    fix: wire up brute-force column key decoding

--- tool/app/Commands/BruteforceColumnKey.php
+++ tool/app/Commands/BruteforceColumnKey.php
@@ -2,8 +2,12 @@
 
 namespace App\Commands;
 
+use App\Actions\Arrays\GeneratePermutation;
+use App\Actions\Ciphers\GeneratePlaintextFromTranspositionCipher;
 use App\Actions\Ciphers\GenerateTranspositionCipherMatrix;
+use App\Actions\Files\ParseWordlistsIntoArray;
 use App\Actions\Strings\GeneratePossibleColumnKeyLengths;
+use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
 
 class BruteforceColumnKey extends Command
@@ -17,9 +21,23 @@ class BruteforceColumnKey extends Command
         $sentence = $this->argument('sentence') ?? $this->ask('Enter a sentence to bruteforce');
         $singleWord = str_replace(' ', '', $sentence);
 
+        $folder = app_path('../../wordlists');
+        $words = ParseWordlistsIntoArray::handle($folder);
+
+        // Strip words too small
+        $dictionaryWords = array_filter($words, function ($word) {
+            return strlen($word) > 5;
+        });
+
         $this->output->write('Sentence: '.$singleWord.PHP_EOL);
         $possibleKeyLengths = GeneratePossibleColumnKeyLengths::handle($singleWord);
         foreach ($possibleKeyLengths as $possibleKeyLength) {
+            $tableHeader = [
+                sprintf('Key (%d)', $possibleKeyLength),
+                'Plaintext',
+            ];
+            $tableData = [];
+
             // Skip 1 or 2 matrix sizes as those are too small to be useful.
             if ($possibleKeyLength < 3) {
                 continue;
@@ -28,10 +46,24 @@ class BruteforceColumnKey extends Command
             // Break the sentence into a matrix of the possible key length.
             $transpositionMatrix = GenerateTranspositionCipherMatrix::handle($singleWord, $possibleKeyLength);
 
-            // Iterate all possible key permutations of that length, looking for English wordlists.
+            // Our starting key will be in form of 0...n
             $startingKey = range(0, $possibleKeyLength - 1);
 
-            $this->output->write('Possible key length: '.$possibleKeyLength.PHP_EOL);
+            // Iterate all possible key permutations of that length, looking for English wordlists.
+            $permutations = GeneratePermutation::handle($startingKey);
+            foreach ($permutations as $permutation) {
+                $plaintext = GeneratePlaintextFromTranspositionCipher::handle($transpositionMatrix, $permutation);
+
+                if (Str::contains($plaintext, $dictionaryWords, ignoreCase: true)) {
+                    $key = implode('', $permutation);
+                    $tableData[$key] = [
+                        'key' => $key,
+                        'plaintext' => $plaintext,
+                    ];
+                }
+            }
+
+            $this->table($tableHeader, $tableData);
         }
 
         return self::SUCCESS;
