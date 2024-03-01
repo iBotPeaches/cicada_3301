a34015d974e0b2b259b9a6abeff5505efc4eb513
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-01 06:32:29 -0500

    test: assertions for column command

---
+++ tool/tests/Feature/BruteforceColumnKeyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class BruteforceColumnKeyTest extends TestCase
+{
+    public function testBruteforceColumnKey(): void
+    {
+        $sentence = 'TL BE IE OV UT HT RE ID TS EO ST PO SO YR SL BT II IY T4 DG UQ IM NU 44 2I 15 33 9M';
+
+        $this->artisan('app:bruteforce-column-key')
+            ->expectsQuestion('Enter a sentence to bruteforce', $sentence)
+            ->expectsTable(['Key (4)', 'Plaintext'], [])
+            ->expectsTable(['Key (7)', 'Plaintext'], [
+                ['0623145', 'TOBELIEVETHUTRISTSDEOTROSPOYSIBTLIIYQ4DTGUI2NUM44IM53139'],
+                ['0625143', 'TOBELIEVETRUTHISTODESTROYPOSSIBILITYQ4UTGDI2N4M4UIM59133']
+            ])
+            ->assertExitCode(0);
+    }
+}
