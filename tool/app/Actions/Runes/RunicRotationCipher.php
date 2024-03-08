e844e6fdc2c2cae60c8fb3ffde9a2371f616f723
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-08 06:58:31 -0500

    feat: shift via runic

--- tool/app/Actions/Runes/RunicRotationCipher.php
+++ tool/app/Actions/Runes/RunicRotationCipher.php
@@ -4,10 +4,36 @@ declare(strict_types=1);
 
 namespace App\Actions\Runes;
 
+use App\Actions\Strings\SplitStringToEnglishRunePairs;
+use App\Enums\Rune;
+
 class RunicRotationCipher
 {
     public static function handle(string $sentence, int $rotation): string
     {
-        // TODO - Rotate the sentence via the implicit index in the runic chart
+        $runeCount = count(Rune::cases());
+        $plaintext = '';
+        $pairs = SplitStringToEnglishRunePairs::handle($sentence);
+        foreach ($pairs as $pair) {
+            // We skip spaces or numbers as those don't exist in Gematria Primus
+            if ($pair === ' ' || is_numeric($pair)) {
+                $plaintext .= ' ';
+
+                continue;
+            }
+
+            // Get the rune from the English letter
+            $rune = Rune::tryFromEnglish($pair);
+            $runicIndex = $rune->toNumericPosition();
+
+            $shiftedIndex = ($runicIndex + $rotation) % $runeCount;
+            $shiftedRune = Rune::tryFromIndex($shiftedIndex);
+
+            if ($shiftedRune) {
+                $plaintext .= $shiftedRune->toSingleLetter();
+            }
+        }
+
+        return $plaintext;
     }
 }
