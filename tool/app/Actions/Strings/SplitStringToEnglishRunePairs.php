2bdba841eae0e80fc362b74e173d80df7c45ee85
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-03-09 07:51:45 -0500

    fix: handle not-yet permuted strings in split

--- tool/app/Actions/Strings/SplitStringToEnglishRunePairs.php
+++ tool/app/Actions/Strings/SplitStringToEnglishRunePairs.php
@@ -10,6 +10,13 @@ class SplitStringToEnglishRunePairs
 {
     public static function handle(string $sentence): array
     {
+        // If the sentence contains a permutation, we generate the permutations and use the first one
+        // This is highly inefficient due to generating all permutations. We should use a generator instead.
+        if (Str::contains($sentence, '[')) {
+            $permutations = GeneratePermutation::handle($sentence);
+            $sentence = $permutations->current();
+        }
+
         $pattern = '/(TH|EO|AE|NG|ING|IA|IO|EA|.)/';
 
         return preg_split($pattern, Str::upper($sentence), -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
