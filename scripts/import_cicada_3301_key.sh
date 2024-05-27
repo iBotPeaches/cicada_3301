e17e83aa638c1033b0b75db0444f0896e99b2381
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-05-27 12:02:41 -0400

    fix: drop ubuntu keyserver for openpgp

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-if gpg2 --keyserver hkps://keyserver.ubuntu.com --recv-keys 67F363C61BA8FB6FDBA9C47D0670B0E57A35090F; then
+if gpg2 --keyserver hkps://keys.openpgp.org --recv-keys 67F363C61BA8FB6FDBA9C47D0670B0E57A35090F; then
     echo "GPG key import successful!"
 else
     echo "GPG key import failed."
