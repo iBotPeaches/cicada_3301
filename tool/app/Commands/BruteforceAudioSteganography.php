6ca090c72bfb6c44519432342825c68554b74cf4
Connor Tumbleson <connor.tumbleson@gmail.com>
2024-11-19 06:56:19 -0500

    feat: initial mp3stego execution

--- tool/app/Commands/BruteforceAudioSteganography.php
+++ tool/app/Commands/BruteforceAudioSteganography.php
@@ -2,9 +2,11 @@
 
 namespace App\Commands;
 
+use App\Actions\Files\FilterWordlists;
 use App\Actions\Process\CheckForDockerImage;
 use App\Services\Docker\StegoToolkit;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
 
 class BruteforceAudioSteganography extends Command
@@ -16,10 +18,10 @@ class BruteforceAudioSteganography extends Command
     public function handle(): int
     {
         $dockerImage = 'dominicbreuker/stego-toolkit';
-        $containerId = CheckForDockerImage::handle($dockerImage);
-        $audioFile = $this->argument('file') ?? storage_path('interconnected.mp3');
+        $imageId = CheckForDockerImage::handle($dockerImage);
+        $audioFile = $this->argument('file') ?? storage_path('mp3stego.mp3');
 
-        if (! $containerId) {
+        if (! $imageId) {
             $this->error('Docker image not found. Please run `docker pull dominicbreuker/stego-toolkit`.');
 
             return self::FAILURE;
@@ -31,7 +33,22 @@ class BruteforceAudioSteganography extends Command
             return self::FAILURE;
         }
 
-        $stegoToolkit = new StegoToolkit($containerId);
+        $wordlistFolder = app_path('../../wordlists');
+        $wordlists = FilterWordlists::handle($wordlistFolder, 1);
+        $stegoToolkit = new StegoToolkit($imageId);
+        $stegoToolkit->addFile($audioFile);
+        $this->info('Attempting to bruteforce audio file using stego-toolkit...');
+        $this->info($stegoToolkit->mp3stegoVersion());
+
+        foreach ($wordlists as $word) {
+            $secret = $stegoToolkit->mp3stego($word);
+
+            if (is_string($secret)) {
+                $this->output->success('SECRET FOUND! on word: '.Str::wrap($word, '"', '"').' using tool: mp3stego.');
+                $this->info($secret);
+                break;
+            }
+        }
 
         return self::SUCCESS;
     }
