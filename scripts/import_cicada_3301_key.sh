5b1fb290fc2e8bcff4b5016242361b3d7b43f025
Connor Tumbleson <connor.tumbleson@gmail.com>
2025-01-21 18:57:17 -0500

    chore: double quote to prevent globbing

--- scripts/import_cicada_3301_key.sh
+++ scripts/import_cicada_3301_key.sh
@@ -5,7 +5,7 @@ set -x
 script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 key="$script_dir/../keys/67F363C61BA8FB6FDBA9C47D0670B0E57A35090F.asc.gpg"
 
-if gpg2 --import $key; then
+if gpg2 --import "$key"; then
     echo "GPG key import successful!"
 else
     echo "GPG key import failed."
